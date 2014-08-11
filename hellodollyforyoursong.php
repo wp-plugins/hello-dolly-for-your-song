14e44437864cdf37973b4105bb12967f518dc037
unmus <unmus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-11 19:38:22 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/hello-dolly-for-your-song/trunk@964053 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hellodollyforyoursong.php
+++ hellodollyforyoursong.php
@@ -2,12 +2,12 @@
 
 /*
 @package Hello_Dolly_For_Your_Song
-@version 0.5
+@version 0.6
 
 Plugin Name: Hello Dolly For Your Song
 Description: This simple plugin is an extended version of the famous hello dolly plugin by Matt Mullenweg. Every human being has a special connection to a particular song. And because of that, Hello Dolly For Your Song brings the lyric of your favourite song to the admin screen, articles and pages.
 Plugin URI: http://wordpress.org/extend/plugins/hello-dolly-for-your-song/
-Version: 0.5
+Version: 0.6
 License: GPLv3
 Author: Marco Hitschler
 Author URI: http://www.unmus.de/
